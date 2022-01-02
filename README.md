fad174a42b3dff6f0d4b1fdf20219a64005abce6
vlaskinmac <kangelina86@mail.ru>
2022-01-02 17:03:36 +0300

    - Created README - Added requirements

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# comics_vk
+# Comics_vk
+
+
+**Script sends comics to [VK](https://vk.com/) group.**
+
+
+This script is written as part of the task of the courses [Devman](https://dvmn.org).
+
+
+<img src="https://user-images.githubusercontent.com/78322994/147877329-8e6127ab-bc69-49ef-8691-6d881ae35f8e.png" alt="drawing" width="650"/>  
+
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine.
+
+### Python Version
+
+Python 3.6 and later.
+
+### Installing
+
+To install the software, you need to install the dependency packages from the file: **requirements.txt**.
+
+Perform the command:
+
+```
+
+pip3 install -r requirements.txt
+
+```
+
+## Getting API keys
+
+**TOKEN**
+
+- Create an application using the link: [`TOKEN`](https://dev.vk.com).
+- Click "Edit".
+- Copy the client_id (id) number from the url.
+- Get the token from the parameter: access_token. 
+- Enter in the url - client_id received in the previous step.
+- Follow the completed url:
+
+https://oauth.vk.com/authorize?client_id={CLIENT_ID}&display=page&scope=photos,groups&response_type=token&v=5.131&state=123456.
+
+**GROUP ID**
+
+- Get the group_id here: [`GROUP ID`](https://regvk.com/id/)
+
+## Launch code
+
+```python
+$ python vk_comics.py
+```
+
+
+## Authors
+
+**vlaskinmac**  - [GitHub-vlaskinmac](https://github.com/vlaskinmac/)
+
+
--- vk_comics.py
+++ vk_comics.py
@@ -1,14 +1,15 @@
+import logging
 import os
-from pprint import pprint
+import random
 
 import requests
-import logging
 
 from dotenv import load_dotenv
 
 load_dotenv()
-token=os.getenv("TOKEN")
-
+token = os.getenv("TOKEN")
+group_id = os.getenv("GROUP_ID")
+VERSION_VK = 5.131
 logging.basicConfig(
         level=logging.WARNING,
         filename="logs.log",
@@ -17,82 +18,61 @@ logging.basicConfig(
     )
 
 
-def get_json_comics():
-    url = "https://xkcd.com/353/info.0.json"
+def get_image_comics():
+    number_comics = random.randint(1, 2560)
+    url = f"https://xkcd.com/{number_comics}/info.0.json"
     response = requests.get(url)
+    image_comics = requests.get(response.json()["img"])
+    with open("comics.png", "wb") as file:
+        file.write(image_comics.content)
+    return response
 
-    print(response.json()['alt'])
 
-    payload = {"access_token": token, "v": 5.131}
-    with open('comics.png', 'rb') as file:
+def get_params_for_save_photo():
+    response = get_image_comics()
+    title = response.json()["alt"]
+    payload = {"access_token": token, "v": VERSION_VK}
+    with open("comics.png", "rb") as file:
         files = {
-            'photo': file,
-
+            "photo": file,
         }
-
-
-
-
         url_for_upload = f"https://api.vk.com/method/photos.getWallUploadServer"
         get_url_for_upload = requests.get(url_for_upload, params=payload)
-        # pprint(get_url_for_upload.json())
-
-
-        get_url_save_photo = get_url_for_upload.json()['response']['upload_url']
+        get_url_save_photo = get_url_for_upload.json()["response"]["upload_url"]
         params_for_save_photo = requests.post(get_url_save_photo, files=files, params=payload)
-        # pprint(params_for_save_photo.json())
+        return params_for_save_photo.json(), title
 
-        payload_save_image = {"access_token": token, "v": 5.131,
-                "hash": params_for_save_photo.json()['hash'],
-                "photo": params_for_save_photo.json()['photo'],
-                "server": params_for_save_photo.json()['server'],
-            }
 
-        # print(params_for_save_photo.json()['photo'])
+def save_photo():
+    params_for_save_photo, _ = get_params_for_save_photo()
+    payload_save_image = {"access_token": token, "v": VERSION_VK,
+            "hash": params_for_save_photo["hash"],
+            "photo": params_for_save_photo["photo"],
+            "server": params_for_save_photo["server"],
+        }
+    url_save_photo = f"https://api.vk.com/method/photos.saveWallPhoto"
+    url_photos = requests.post(url_save_photo, params=payload_save_image)
+    return url_photos.json()
 
-        url_save_photo = f"https://api.vk.com/method/photos.saveWallPhoto"
-        get_url_for_upload = requests.post(url_save_photo, params=payload_save_image)
-        # pprint(get_url_for_upload.json())
 
-        payload_wall = {"access_token": token, "v": 5.131,
+def get_id_numbers():
+    url_photos = save_photo()
+    _, title = get_params_for_save_photo()
+    id_numbers = [(id_number["id"], id_number["owner_id"]) for id_number in url_photos["response"]]
+    for unpacking_id_number in id_numbers:
+        photo_id, owner_id = unpacking_id_number
+        signed = 1
+        payload_wall = {"access_token": token, "v": VERSION_VK,
                               "filter": "suggests, postponed",
-                              "from_group": 1,
-                              # "group_id": 209816900,
-                              # "owner_id": -8037916,
-                              "owner_id": -209816900,
-                              "attachments": "photo695777185_457239022",
-                              "message": f"{response.json()['alt']}",
-
+                              "from_group": signed,
+                              "owner_id": group_id,
+                              "attachments": f"photo{owner_id}_{photo_id}",
+                              "message": title,
                               }
 
         url_wall_get = f"https://api.vk.com/method/wall.post"
-        wall_get = requests.post(url_wall_get, params=payload_wall)
-        pprint(wall_get.json())
+        requests.post(url_wall_get, params=payload_wall)
+        os.remove("./comics.png")
 
 
-
-        # url2 = f"https://api.vk.com/method/photos.getUploadServer"
-
-
-        #
-
-        # pprint(response2.json()['response']['upload_url'])
-
-
-
-        #
-        #
-        #
-        # response3 = requests.post(url, params=payload_save_image)
-
-
-
-        # response.raise_for_status()
-        # logging.warning(response.status_code)
-
-
-
-        # pprint(response2.json())
-        # print(response.url)
-        # return response.json()
-get_json_comics()
+get_id_numbers()
