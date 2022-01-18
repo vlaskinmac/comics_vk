608ddf71e0e7f0b76849922ed0427320e6ddaa3b
vlaskinmac <kangelina86@mail.ru>
2022-01-18 13:08:12 +0300

    Renamed get_image_comics()

--- vk_comics.py
+++ vk_comics.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import random
+from pprint import pprint
 
 import requests
 
@@ -15,34 +16,43 @@ def get_image_comics_content():
     return image_comics_content.json()
 
 
-def get_image_comics():
+def get_image_title_comics():
     collection_images = get_image_comics_content()
     image_comics = requests.get(collection_images["img"])
+    title_comics = requests.get(collection_images["alt"])
     image_comics.raise_for_status()
     with open("comics.png", "wb") as file:
         file.write(image_comics.content)
+    return title_comics
 
 
 def get_files_photos():
     with open("comics.png", "rb") as file:
         try:
             files = {
-                "photo": file,
+                "photo": file.read(),
             }
+
         finally:
-            os.remove("./comics.png")
+            pass  # +
+            # os.remove("./comics.png")
+
     return files
 
 
 def get_content_for_save_photo():
+    title = get_image_title_comics() # +
     files = get_files_photos()
-    collection_photo = get_image_comics_content()
-    title = collection_photo["alt"]
+    # collection_photo = get_image_comics_content()
+    # title = collection_photo["alt"]
+
     payload = {"access_token": vk_token, "v": VERSION_VK}
     url_for_upload = f"https://api.vk.com/method/photos.getWallUploadServer"
+    # print(url_for_upload)
     get_url_for_upload = requests.get(url_for_upload, params=payload)
     get_url_for_upload.raise_for_status()
     get_url_save_photo = get_url_for_upload.json()["response"]["upload_url"]
+    # pprint(get_url_for_upload.json())
     params_for_save_photo = requests.post(get_url_save_photo, files=files, params=payload)
     params_for_save_photo.raise_for_status()
     return params_for_save_photo.json(), title
@@ -50,21 +60,32 @@ def get_content_for_save_photo():
 
 def get_content_url_photos():
     params_for_save_photo, _ = get_content_for_save_photo()
+    # pprint(params_for_save_photo)
     payload_save_image = {
-        "access_token": vk_token, "v": VERSION_VK,
+        "access_token": vk_token,
+        "v": VERSION_VK,
         "hash": params_for_save_photo["hash"],
         "photo": params_for_save_photo["photo"],
         "server": params_for_save_photo["server"],
     }
+    # pprint(payload_save_image)
     url_save_photo = f"https://api.vk.com/method/photos.saveWallPhoto"
     url_photos = requests.post(url_save_photo, params=payload_save_image)
     url_photos.raise_for_status()
+
+    pprint(url_photos.json())
     return url_photos.json()
+
 
 
 def posts_comics():
     url_photos = get_content_url_photos()
+    # pprint(url_photos)
+
+
+
     _, title = get_content_for_save_photo()
+
     id_numbers = [(id_number["id"], id_number["owner_id"]) for id_number in url_photos["response"]]
     for unpacking_id_number in id_numbers:
         photo_id, owner_id = unpacking_id_number
